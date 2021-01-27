b5956a1df05fff926b2bd0336fbf3a7e62bcf7a4
Anri Digholm <anridig@gmail.com>
2021-01-27 18:45:27 +0900

    refactor: Separated code somewhat and added support for edge cases.

--- main.go
+++ main.go
@@ -47,8 +47,6 @@ func main() {
 
 	pflag.Parse()
 
-	var myUserID string
-
 	// Try loading token from env var if no --token flag was passed.
 	if *token == "" {
 		*token = os.Getenv("MY_SLACK_TOKEN")
@@ -89,153 +87,19 @@ func main() {
 
 	api := slack.New(*token)
 
-	// Fetch users.
-	users := make(map[string]string)
+	// Get all users in workspace.
+	users, myUserID := getUsers(api, *me)
 
-	{
-		fmt.Println("Fetching users ..")
+	// Get all files.
+	files := getFiles(api, myUserID, beforeTSUnix)
 
-		res, err := api.GetUsers()
-		if err != nil {
-			panic(err)
-		}
+	// Get channels.
+	channels := getChannels(api, pattern, users)
 
-		for _, u := range res {
-			if u.Name == *me {
-				myUserID = u.ID
-			}
-			users[u.ID] = u.Name
-		}
+	// Get messages.
+	messages := getMessages(api, channels, myUserID, beforeTS)
 
-		if myUserID == "" {
-			panic("could not find user: " + *me)
-		}
-
-		fmt.Printf("Found myself: ID: %s, Name: %s\n", myUserID, users[myUserID])
-	}
-
-	// Fetch files.
-	var filesToDelete []slack.File
-	{
-		var found int
-
-		p := &slack.ListFilesParameters{
-			User: myUserID,
-		}
-
-		for {
-			var res []slack.File
-			var err error
-
-			res, p, err = api.ListFiles(*p)
-			if err != nil {
-				panic(err)
-			}
-
-			for _, f := range res {
-				if f.Created < beforeTSUnix {
-					found++
-					fmt.Printf("%03d. Found file: %s (created: %s)\n", found, f.Name, f.Created.Time().String())
-					filesToDelete = append(filesToDelete, f)
-				}
-			}
-
-			if p.Cursor == "" {
-				break
-			}
-
-			fmt.Printf("Next cursor: %s (files: %d)\n", p.Cursor, len(filesToDelete))
-		}
-
-		fmt.Printf("Found %d files.\n", len(filesToDelete))
-	}
-
-	// Fetch channels.
-	var channels []slack.Channel
-	{
-		var found int
-		var cursor string
-
-		for {
-			res, next, err := api.GetConversations(&slack.GetConversationsParameters{
-				Types:  []string{"private_channel", "mpim", "im"},
-				Cursor: cursor,
-			})
-			if err != nil {
-				panic(err)
-			}
-
-			for _, c := range res {
-				if c.IsPrivate || c.IsMpIM {
-					if pattern.MatchString(c.Name) {
-						found++
-						fmt.Printf("%03d. CH ID: %s, Name: %s\n", found, c.ID, c.Name)
-						channels = append(channels, c)
-					}
-				}
-				if c.IsIM {
-					username := users[c.User]
-					if pattern.MatchString(username) {
-						found++
-						fmt.Printf("%03d. IM ID: %s, Name: %s\n", found, c.ID, username)
-						channels = append(channels, c)
-					}
-				}
-			}
-
-			if next == "" {
-				break
-			}
-
-			fmt.Printf("Next cursor: %s (channels: %d)\n", next, len(channels))
-			cursor = next
-		}
-
-		fmt.Printf("Found %d channels.\n", len(channels))
-	}
-
-	// Fetch messages.
-	var messagesToDelete []slack.Message
-	{
-		var found int
-
-		for _, c := range channels {
-
-			var cursor string
-			for {
-				res, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{
-					ChannelID: c.ID,
-					Cursor:    cursor,
-				})
-				if err != nil {
-					panic(err)
-				}
-
-				for _, m := range res.Messages {
-					if m.User == myUserID {
-						if beforeTS != "" && m.Timestamp > beforeTS {
-							continue
-						}
-
-						found++
-						fmt.Printf("%03d. MSG TS: %s, Text: %s\n", found, fromSlackTimestamp(m.Timestamp), m.Text)
-
-						m.Channel = c.ID
-						messagesToDelete = append(messagesToDelete, m)
-					}
-				}
-
-				if !res.HasMore {
-					break
-				}
-
-				cursor = res.ResponseMetaData.NextCursor
-				fmt.Printf("Next cursor: %s (messages: %d)\n", cursor, len(res.Messages))
-			}
-		}
-	}
-
-	fmt.Printf("\nFound %d messages and %d files to delete!\n\n", len(messagesToDelete), len(filesToDelete))
+	fmt.Printf("\nFound %d messages and %d files to delete!\n\n", len(messages), len(files))
 
 	if !*commit {
 		fmt.Printf("Run command again with --commit flag to perform the delete operation!\n\n")
@@ -243,54 +107,10 @@ func main() {
 	}
 
 	// Delete messages.
-	{
-		for i, m := range messagesToDelete {
-			for {
-				ch, ts, err := api.DeleteMessage(m.Channel, m.Timestamp)
-				if err != nil {
-					if !strings.Contains(err.Error(), "rate limit") {
-						fmt.Printf("Delete failed: %s (message: ID: %s, Text: %s)\n", err.Error(), m.Timestamp, m.Text)
-						break
-					}
-
-					// We were rate limited, wait a sec and retry!
-					fmt.Printf("Rate limited, retrying in 1 sec ..\n")
-					time.Sleep(1000 * time.Millisecond)
-					continue
-				}
-
-				fmt.Printf("%03d. Deleted message: %s %s\n", i+1, ch, ts)
-				break
-			}
-
-			time.Sleep(200 * time.Millisecond)
-		}
-	}
+	deleteMessages(api, messages)
 
 	// Delete files.
-	{
-		for i, f := range filesToDelete {
-			for {
-				err := api.DeleteFile(f.ID)
-				if err != nil {
-					if !strings.Contains(err.Error(), "rate limit") {
-						fmt.Printf("Delete failed: %s (file: ID: %s, Name: %s)\n", err.Error(), f.ID, f.Name)
-						break
-					}
-
-					// We were rate limited, wait a sec and retry!
-					fmt.Printf("Rate limited, retrying in 1 sec ..\n")
-					time.Sleep(1000 * time.Millisecond)
-					continue
-				}
-
-				fmt.Printf("%03d. Deleted file: %s (created: %s)\n", i+1, f.Name, f.Created.Time().String())
-				break
-			}
-
-			time.Sleep(200 * time.Millisecond)
-		}
-	}
+	deleteFiles(api, files)
 
 	fmt.Printf("\nIt's a Done Deal!\n\n")
 }
@@ -305,4 +125,241 @@ func fromSlackTimestamp(ts string) time.Time {
 func toSlackTimestamp(t time.Time) string {
 	ts := fmt.Sprintf("%d", t.UnixNano()/1000)
 	return fmt.Sprintf("%s.%s", ts[0:len(ts)-6], ts[len(ts)-6:])
+}
+
+func getUsers(api *slack.Client, me string) (users map[string]string, myUserID string) {
+	users = make(map[string]string)
+
+	fmt.Println("Loading all users in workspace ..")
+
+	res, err := api.GetUsers()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, u := range res {
+		if u.Name == me {
+			myUserID = u.ID
+		}
+		users[u.ID] = u.Name
+	}
+
+	if myUserID == "" {
+		panic("could not find user: " + me)
+	}
+
+	fmt.Printf("Found myself: ID: %s, Name: %s\n", myUserID, users[myUserID])
+	return
+}
+
+func getFiles(api *slack.Client, myUserID string, before slack.JSONTime) (filesToDelete []slack.File) {
+	var found int
+
+	p := &slack.ListFilesParameters{
+		User: myUserID,
+	}
+
+	for {
+		var res []slack.File
+		var err error
+
+		res, p, err = api.ListFiles(*p)
+		if err != nil {
+			if !strings.Contains(err.Error(), "rate limit") {
+				panic(err)
+			}
+
+			fmt.Printf("Rate limited, retrying in 1 sec ..\n")
+			time.Sleep(1000 * time.Millisecond)
+		}
+
+		for _, f := range res {
+			if f.Created < before {
+				found++
+				fmt.Printf("%03d. Found file: %s (created: %s)\n", found, f.Name, f.Created.Time().String())
+				filesToDelete = append(filesToDelete, f)
+			}
+		}
+
+		if p.Cursor == "" {
+			break
+		}
+
+		fmt.Printf("Next cursor: %s (files: %d)\n", p.Cursor, len(filesToDelete))
+	}
+
+	fmt.Printf("Found %d files.\n", len(filesToDelete))
+	return
+}
+
+func getChannels(api *slack.Client, filter *regexp.Regexp, users map[string]string) (channels []slack.Channel) {
+	var found int
+	var cursor string
+
+	for {
+		res, next, err := api.GetConversations(&slack.GetConversationsParameters{
+			Types:  []string{"private_channel", "mpim", "im"},
+			Cursor: cursor,
+		})
+		if err != nil {
+			if !strings.Contains(err.Error(), "rate limit") {
+				panic(err)
+			}
+
+			fmt.Printf("Rate limited, retrying in 1 sec ..\n")
+			time.Sleep(1000 * time.Millisecond)
+			continue
+		}
+
+		for _, c := range res {
+			if c.IsMpIM {
+				// Include all MpIM channels if there's no filter.
+				if filter == nil || filter.MatchString(c.Name) {
+					found++
+					fmt.Printf("%03d. MpIM ID: %s, Name: %s\n", found, c.ID, c.Name)
+					channels = append(channels, c)
+				}
+			}
+			if c.IsIM {
+				username := users[c.User]
+				// Include all DM channels if there's no filter.
+				if filter == nil || filter.MatchString(username) {
+					found++
+					fmt.Printf("%03d. IM ID: %s, Name: %s\n", found, c.ID, username)
+					channels = append(channels, c)
+				}
+			}
+			if c.IsPrivate {
+				// Skip all other private channels if there's no filter.
+				if filter != nil && filter.MatchString(c.Name) {
+					found++
+					fmt.Printf("%03d. CH ID: %s, Name: %s\n", found, c.ID, c.Name)
+					channels = append(channels, c)
+				}
+			}
+		}
+
+		if next == "" {
+			break
+		}
+
+		fmt.Printf("Next cursor: %s (channels: %d)\n", next, len(channels))
+		cursor = next
+	}
+
+	fmt.Printf("Found %d channels.\n", len(channels))
+	return
+}
+
+func getMessages(api *slack.Client, channels []slack.Channel, myUserID, beforeTS string) (messagesToDelete []slack.Message) {
+	var found int
+
+	for _, c := range channels {
+
+		var cursor string
+		var last int
+
+		for {
+			res, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{
+				ChannelID: c.ID,
+				Cursor:    cursor,
+				Limit:     1000,
+			})
+			if err != nil {
+				if !strings.Contains(err.Error(), "rate limit") {
+					panic(err)
+				}
+
+				fmt.Printf("Rate limited, retrying in 1 sec ..\n")
+				time.Sleep(1000 * time.Millisecond)
+				continue
+			}
+
+			for _, m := range res.Messages {
+				if m.User == myUserID {
+					if beforeTS != "" && m.Timestamp > beforeTS {
+						continue
+					}
+
+					if found == last {
+						fmt.Println("")
+					}
+
+					found++
+
+					channelName := c.Name
+					if channelName == "" {
+						channelName = c.ID
+					}
+
+					fmt.Printf("%03d. CH: %s MSG TS: %s, Text: %s\n", found, channelName, fromSlackTimestamp(m.Timestamp), m.Text)
+
+					m.Channel = c.ID
+					messagesToDelete = append(messagesToDelete, m)
+				}
+			}
+
+			if !res.HasMore {
+				break
+			}
+
+			cursor = res.ResponseMetaData.NextCursor
+
+			if found == last {
+				fmt.Printf(".")
+			} else {
+				fmt.Printf("Reading: %s\n", cursor)
+			}
+			last = found
+		}
+	}
+
+	return
+}
+
+func deleteMessages(api *slack.Client, ms []slack.Message) {
+	for i, m := range ms {
+
+		for {
+			ch, ts, err := api.DeleteMessage(m.Channel, m.Timestamp)
+			if err != nil {
+				if !strings.Contains(err.Error(), "rate limit") {
+					fmt.Printf("Delete failed: %s (message: ID: %s, Text: %s)\n", err.Error(), m.Timestamp, m.Text)
+					break
+				}
+
+				fmt.Printf("Rate limited, retrying in 1 sec ..\n")
+				time.Sleep(1000 * time.Millisecond)
+				continue
+			}
+
+			fmt.Printf("%03d. Deleted message: %s %s\n", i+1, ch, ts)
+			break
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func deleteFiles(api *slack.Client, fs []slack.File) {
+	for i, f := range fs {
+		for {
+			err := api.DeleteFile(f.ID)
+			if err != nil {
+				if !strings.Contains(err.Error(), "rate limit") {
+					fmt.Printf("Delete failed: %s (file: ID: %s, Name: %s)\n", err.Error(), f.ID, f.Name)
+					break
+				}
+
+				fmt.Printf("Rate limited, retrying in 1 sec ..\n")
+				time.Sleep(1000 * time.Millisecond)
+				continue
+			}
+
+			fmt.Printf("%03d. Deleted file: %s (created: %s)\n", i+1, f.Name, f.Created.Time().String())
+			break
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
 }
